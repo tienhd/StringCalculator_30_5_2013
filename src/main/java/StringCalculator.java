8222c6ae46d9bb31eef59c8272bf4fcae328fa1a
Tien <Hoang>
2013-05-30 14:46:39 +0700

    update

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -14,14 +14,15 @@ public class StringCalculator {
         }
         else {
             String regex = "([,\n//;]*)";
+            String negative = "(-[\\d]*)";
             String [] numberString = inputString.split(regex);
             for (String s : numberString) {
                 if (!s.isEmpty()) {
                     //System.out.println(s + " ");
-                    int number = Integer.parseInt(s);
-                    if (number < 0) {
+                    if (s.matches(negative)) {
                         throw new NumberFormatException("Negative value not allowed");
                     }
+                    int number = Integer.parseInt(s);
                     result += number;
                 }
             }
